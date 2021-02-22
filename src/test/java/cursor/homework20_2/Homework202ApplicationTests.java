2047d0bc4efd9aafb55888b7b64935af8d9aa571
Dmytro <65898747+fill19@users.noreply.github.com>
2021-02-22 23:52:43 +0200

    Initial commit

---
+++ src/test/java/cursor/homework20_2/Homework202ApplicationTests.java
@@ -0,0 +1,13 @@
+package cursor.homework20_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework202ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
